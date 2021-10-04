c4e6408346bfbd173e6c5b2cfee62527a0521373
Mourad <mourad777b@gmail.com>
2021-10-04 07:58:35 -0400

    moved database password to .env

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('messenger', 'postgres', 'Bugsbunny777!', {
+const db = new Sequelize('messenger', 'postgres', process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect:'postgres'
 });
