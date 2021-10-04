4f7a67a5e82f72a8285985260acfd4334eef6b03
Mourad <mourad777b@gmail.com>
2021-10-04 08:01:49 -0400

    moved db credentials to .env

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('messenger', 'postgres', process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect:'postgres'
 });
