7ea0a63043540957b5cadbe50653b5211d9aba17
Mourad <mourad777b@gmail.com>
2021-10-10 22:32:20 -0400

    note

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -10,7 +10,8 @@ Conversation.belongsToMany(User);
 Conversation.hasMany(Message);
 Message.belongsTo(Conversation);
 
-Conversation.belongsTo(User, { foreignKey: 'userId', as: 'admin' })
+//group conversations have 1 admin that can add/remove other users from the convo
+Conversation.belongsTo(User, { foreignKey: 'userId', as: 'admin' });
 
 module.exports = {
   User,
